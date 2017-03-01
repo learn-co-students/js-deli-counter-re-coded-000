0ee5fb966699761ae9e5d1c6ca59e1d87e98d3ef
dina aziz <dina.aziz100@yahoo.com>
2017-03-01 09:55:39 +0000

    Done.

---
+++ deli.js
@@ -0,0 +1,24 @@
+var katzDeli = [];
+function takeANumber(katzDeli, name) {
+  katzDeli.push(name);
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`;
+}
+
+
+function nowServing(katzDeli) {
+  return "hi";
+  // return katzDeli.length > 0 ? `Currently serving ${katzDeli.slice(0,1)}.` : "There is nobody waiting to be served!";
+}
+
+function currentLine(katzDeli) {
+  if (katzDeli.length == 0) {
+    return "The line is currently empty.";
+  } else {
+    var numAndNames = []
+    katzDeli.forEach(function(name, index) {
+      var number = index+1
+      numAndNames.push(`${number}. ${name}`)
+    });
+ 	  return `The line is currently: ${numAndNames.join(", ")}`;
+  }
+}
